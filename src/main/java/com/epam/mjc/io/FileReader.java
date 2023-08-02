bb8fa343c21cbb6451ce27db60bb83fca0dc6ad7
Makar <hoteng789@gmail.com>
2023-08-02 20:50:19 +0400

    Input Stream Java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,51 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        String fileContent = null;
+        int symbol;
+        try (java.io.FileReader inputStream = new java.io.FileReader(file.getAbsolutePath())) {
+            while ((symbol = inputStream.read()) != -1) {
+                fileContent = fileContent == null ? String.valueOf((char) symbol) : fileContent + (char) symbol;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+
+        }
+
+        String[] parseString = getParseString(fileContent);
+
+        String name = getValueByKey(parseString, "Name");
+        String email = getValueByKey(parseString, "Email");
+        Long phone = Long.parseLong(getValueByKey(parseString, "Phone"));
+        Integer age = Integer.parseInt(getValueByKey(parseString, "Age"));
+
+
+        return new Profile(name, age, email, phone);
     }
+
+    private String[] getParseString(String inputString) {
+        return inputString.split(System.lineSeparator());
+
+    }
+
+    private String getValueByKey(String[] inputString, String key) {
+
+        for (int i = 0; i < inputString.length; i++) {
+            if (inputString[i].contains(key)) {
+                return inputString[i].substring(key.length() + 2);
+            }
+        }
+
+        return key;
+    }
+
 }
